563c718cacb065b796c64402fe85bff56a4c4472
Jay-SCM <John.Farmer1086@gmail.com>
2023-11-21 22:15:20 +0930

    added card for the emoji

--- vite-project/src/components/Emoji.jsx
+++ vite-project/src/components/Emoji.jsx
@@ -2,12 +2,13 @@
 import React, { useState } from 'react';
 import happyEmoji from '../assets/happy.jpg';
 import sadEmoji from '../assets/sad.jpg';
+import './HappySad.css';
 const Emoji = () => {
     const [isHappy, setIsHappy] = useState(true);
     const toggleMood = () => {
         setIsHappy((prevIsHappy) => !prevIsHappy);};
     return (
-        <div>
+        <div className="happyCard">
             <img
                 src={isHappy ? happyEmoji : sadEmoji}
                 alt={isHappy ? 'Happy Emoji' : 'Sad Emoji'}
